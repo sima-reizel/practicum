c8f8485038f9703a7cfcc734b2c3bbf98e9b0fbd
sima-reizel <sima.glick@gmail.com>
2023-05-15 23:13:39 +0300

    add useTransition

--- src/image.js
+++ src/image.js
@@ -1,15 +1,29 @@
 import { useDispatch, useSelector } from "react-redux";
 import {fetchAllImages} from './features/imageSlice';
 import { useEffect } from "react";
+import { useState } from "react";
 
 export default function Image(){
+  const [isLoading, setIsLoading] = useState(false);
+
     let disp=useDispatch();
-    useEffect(
-        ()=>{disp(fetchAllImages())},[]
-    )
+    
+
+    useEffect(() => {
+      setIsLoading(true);
+        disp(fetchAllImages())
+        
+        setIsLoading(false);     
+    },);
+    
     let imageArr1=useSelector(state=>state.image.imageArr)
 
-return(<>
-  {imageArr1.map(item=><div key={item.id}>{item.url}</div>)}
+return(
+<>
+ {isLoading? (<p>Loading...</p>) : (<><h2>Data:</h2>
+  {imageArr1&&
+  imageArr1.map(item=><div key={item.id}>{item.url}</div>)
+  }
+</>)}
 </>)
 }
